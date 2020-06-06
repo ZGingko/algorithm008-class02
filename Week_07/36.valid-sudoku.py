759e4ebb4b9418acdfde12e2c0ff115a2e4af097
ZGingko <zhengcanping147@sina.com>
2020-06-07 05:01:38 +0800

    有效的数独

---
+++ Week_07/36.valid-sudoku.py
@@ -0,0 +1,49 @@
+from typing import List
+import collections
+
+
+class Solution: 
+    """
+    36.有效的数独 https://leetcode-cn.com/problems/valid-sudoku/
+    """
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        if not board or not board[0]:
+            return False
+        # init data
+        rows = [{} for i in range(9)]
+        columns = [{} for i in range(9)]
+        boxes = [{} for i in range(9)]
+
+        # validate a board
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num != '.':
+                    num = int(num)
+                    box_index = (i // 3 ) * 3 + j // 3
+                    
+                    # keep the current cell value
+                    rows[i][num] = rows[i].get(num, 0) + 1
+                    columns[j][num] = columns[j].get(num, 0) + 1
+                    boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
+                    
+                    # check if this value has been already seen before
+                    if rows[i][num] > 1 or columns[j][num] > 1 or boxes[box_index][num] > 1:
+                        return False         
+        return True
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    board = [
+                ["5","3",".",".","7",".",".",".","."],
+                ["6",".",".","1","9","5",".",".","."],
+                [".","9","8",".",".",".",".","6","."],
+                ["8",".",".",".","6",".",".",".","3"],
+                ["4",".",".","8",".","3",".",".","1"],
+                ["7",".",".",".","2",".",".",".","6"],
+                [".","6",".",".",".",".","2","8","."],
+                [".",".",".","4","1","9",".",".","5"],
+                [".",".",".",".","8",".",".","7","9"]
+            ]
+    print(sol.isValidSudoku(board))
